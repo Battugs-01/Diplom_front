f01aae072bb5921e395640c1f246eddc7d063f5d
tugsking2002 <tugsking2002@gmail.com>
2024-05-06 08:02:39 -0700

    [prod]

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ module.exports = withTM({
     // HOST_API_KEY: 'https://api-dev-minimal-v4.vercel.app',
     // HOST_API_KEY: 'https://time-order.onrender.com',
     HOST_API_KEY: 'http://103.151.141.33:5000',
-    HOST_API_KEY_PROD: 'https://time-order.onrender.com',
+    HOST_API_KEY_PROD: 'http://103.151.141.33:5000',
     HOST_FILE_KEY: 'https://time-order.onrender.com',
     HOST_API_KEY_TEST: 'http://43.231.112.191:8070/v1',
     HOST_API_KEY_TEST: 'https://time-order.onrender.com',
